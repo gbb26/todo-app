a9f55c2352677887c00a89b5c48dc414ac6cf6ac
Gorakshnath <gorakshnath107@gmail.com>
2023-04-13 16:54:22 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,92 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
-
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  // STATES
+  const [newItem,setNewItem] = useState("")
+  const [todos,setTodos] = useState(() =>{
+    const localVal = localStorage.getItem("Items")
+    if(localVal == null) return []
+    return JSON.parse(localVal)
+  })
+  // HANDLES SUBMIT
+  const handleSubmit = (event)=>{
+    event.preventDefault()
+    setTodos((current) =>{
+      return [
+        ...current,{
+          id:crypto.randomUUID()
+          ,title:newItem,completed:false},
+      ]
+    })
+    setNewItem("")
+  }
+  // TOGGLES CHECKBOX
+  const toggle = (id,completed) =>{
+    setTodos((current) =>{
+      return current.map(todo =>{
+        if(todo.id === id)
+        {
+          todo.completed = completed
+          return {...todo,completed}
+        }
+        return todo
+      })
+    })
+  }
+  // DELETES TODO
+  const deleteTodo = (id) =>{
+    setTodos(current =>{
+      return current.filter((todo) => todo.id !== id
+      )
+    })
+  }
+  // useEffect Hook
+  useEffect(() =>{
+    localStorage.setItem("Items",JSON.stringify(todos))
+  },[todos])
+   return (
+    <div className='main'>
+    <form
+    onSubmit={handleSubmit}
+    className='new-item-form'>
+      <div className='form-row'>
+        <input type='text' id='item' 
+        placeholder='Add Item...'
+        value={newItem}
+        onChange={(event) =>{
+          setNewItem(event.target.value)
+        }}
+        />
+      </div>
+      <button className='btn submit-button'>Add</button>
+    </form>
+    <h1 className='header'>Todo List</h1>
+    <ul className='list'>
+      {todos.length === 0 && "No TODOS"}
+      {
+        todos.map((items) =>{
+          return(
+          <li>
+          <label key={items.id}>
+          <input className='checkbox' type='checkbox'
+           checked={items.completed}
+           onChange={(event) =>{
+            toggle(items.id,event.target.checked)
+           }}
+           />
+          <span>{items.title}</span>
+          </label>
+          <button
+          onClick={()=>deleteTodo(items.id)}
+          className='btn btn-danger'>Delete</button>
+        </li>
+        )
+        })
+      }
+
+    </ul>
     </div>
-  );
+  )
 }
 
 export default App;
